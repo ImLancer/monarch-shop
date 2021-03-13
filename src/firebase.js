761348fa63ef2743a071c311ccebda264012f182
ImLancer <dev.imlancer@gmail.com>
2021-03-13 20:40:16 +0700

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAN-2qp4YxtUL7_JDr8QUx0Qz56rhJiB60",
+  authDomain: "monarch-project-plan.firebaseapp.com",
+  databaseURL: "https://monarch-project-plan-default-rtdb.firebaseio.com",
+  projectId: "monarch-project-plan",
+  storageBucket: "monarch-project-plan.appspot.com",
+  messagingSenderId: "701411787455",
+  appId: "1:701411787455:web:80d6f9d22a185e5b61bc0a",
+  measurementId: "G-ZG1EK6NXC0"
+};
