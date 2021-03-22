933a62cee0aaf609e9254fe6a7f326e7f69aab6e
ImLancer <dev.imlancer@gmail.com>
2021-03-23 00:47:58 +0700

    delete some sp

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -30,30 +30,6 @@ function Row() {
         img="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg"
         color={color}
       />
-      <Product 
-        title="Blue T-Shirt"
-        price="150.000 VNĐ"
-        img="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg"
-        color={color}
-      />
-      <Product 
-        title="Blue T-Shirt"
-        price="150.000 VNĐ"
-        img="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg"
-        color={color}
-      />
-      <Product 
-        title="Blue T-Shirt"
-        price="150.000 VNĐ"
-        img="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg"
-        color={color}
-      />
-      <Product 
-        title="Blue T-Shirt"
-        price="150.000 VNĐ"
-        img="https://imgprd19.hobbylobby.com/9/5f/26/95f264323ae49e65b2a53a909fcd7d9ee659f3c7/350Wx350H-422519-0320.jpg"
-        color={color}
-      />
     </div>
   )
 }
